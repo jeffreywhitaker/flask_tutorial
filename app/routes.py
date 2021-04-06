2a97fe34a3093cb207501df24221f13671a37e52
Jeffrey Whitaker <jeffrey.david.whitaker@zoho.com>
2021-04-05 17:39:51 -0700

    add loops and conditionals

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,18 @@
-from falsk import render_template
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'username': 'Jeff'}
-    return render_template('index.html', title="Home", user=user)
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title="Home", user=user, posts=posts)
